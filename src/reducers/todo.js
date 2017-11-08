e9acb1139a10f6d09cf96b582f13cbdee8cf644e
Matloob Hasan <matloob@caloriecloud.org>
2017-11-08 11:48:08 +0500

    dispatch asynchronous actions with redux-thunk middleware

--- src/reducers/todo.js
+++ src/reducers/todo.js
@@ -1,18 +1,26 @@
+import {getTodos} from "../lib/todoServices";
+
 const initState = {
-  todos: [
-    {id: 1, name: 'Render static UI', isComplete: true},
-    {id: 2, name: 'Create initial state', isComplete: true},
-    {id: 3, name: 'Render based on state', isComplete: true}
-  ],
+  todos: [],
   currentTodo: ''
 };
 
-const TODO_ADD = 'TODO_ADD';
-const CURRENT_UPDATE = 'CURRENT_UPDATE';
+const TODO_ADD        = 'TODO_ADD';
+const CURRENT_UPDATE  = 'CURRENT_UPDATE';
+const TODOS_LOAD      = 'TODOS_LOAD';
 
 
 export const updateCurrentAction = (payload) => ({ type: CURRENT_UPDATE, payload: payload });
+export const loadTodosAction     = (payload) => ({ type: TODOS_LOAD, payload: payload });
 
+
+export const fetchTodos = () => {
+  return (dispatch) => {
+    getTodos().then((todos) => {
+      dispatch(loadTodosAction(todos));
+    });
+  }
+};
 
 export default (state = initState, action) => {
 
@@ -22,6 +30,8 @@ export default (state = initState, action) => {
     case TODO_ADD:
       //console.debug("Adding Todo:", action.payload.name);
       return {...state, todos: state.todos.concat(action.payload)};
+    case TODOS_LOAD:
+      return {...state, todos: action.payload};
     default:
       return state;
   }
--- src/store.js
+++ src/store.js
@@ -1,4 +1,9 @@
-import {createStore} from 'redux';
-import reducer from './reducers/todo';
+import {createStore, applyMiddleware} from 'redux'
+import thunk from 'redux-thunk'
+import reducer from './reducers/todo'
 
-export default createStore(reducer);
+export default createStore(
+  reducer,
+  applyMiddleware(thunk)
+)
+ 
