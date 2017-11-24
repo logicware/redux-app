7bc6f9cfa98840414981016c53a4791645907206
Matloob Hasan <matloob@caloriecloud.org>
2017-11-24 10:33:37 +0500

    add react router to the redux app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {BrowserRouter as Router} from 'react-router-dom';
 
 import logo from './logo.svg';
 import './App.css';
@@ -6,6 +7,7 @@ import './App.css';
 import TodoForm from './components/TodoForm';
 import TodoList from './components/TodoList';
 import Message  from './components/Message';
+import Footer   from './components/Footer';
 
 class App extends Component {
   render() {
@@ -16,13 +18,14 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React with Redux</h2>
         </div>
-
-        <div className="Todo-App">
-          <Message />
-          <TodoForm />
-          <TodoList />
-        </div>
-
+        <Router>
+          <div className="Todo-App">
+            <Message />
+            <TodoForm />
+            <TodoList />
+            <Footer/>
+          </div>
+        </Router>
       </div>
     );
   }
